cd8fe10ce0a0dd881943688fc92e9398234a5807
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-02-27 12:49:05 +0800

    Contact Form Built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const addAppointment = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        title: title,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
+
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
 
   return (
     <>
@@ -36,12 +52,14 @@ function App() {
             <Redirect to={ROUTES.CONTACTS} />
           </Route>
           <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} addContact={addContact} />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
-            {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage
+              appointments={appointments}
+              addAppointment={addAppointment}
+              contacts={contacts}
+            />
           </Route>
         </Switch>
       </main>
--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,19 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input type='text' value={name} name='name' onChange={(e) => setName(e.target.value)} required placeholder='Contact Name' />
+      </label>
+      <label>
+        <input type='tel' value={phone} name='phone' onChange={(e) => setPhone(e.target.value)} required placeholder='Phone Number'/>
+      </label>
+      <label>
+        <input type='email' value={email} name='email' onChange={(e) => setEmail(e.target.value)} required placeholder='Email' />
+      </label>
+      <label>
+        <input type='submit' />
+      </label>
+    </form>
   );
 };
